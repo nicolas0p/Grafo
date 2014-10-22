28f170e05db104a70d641b9a926a636ec7dc4b23
Nicolas Pfeifer <nicolas0pfeifer@gmail.com>
2014-10-22 18:05:37 -0200

    Adicionada checagem se grafo e regular, assim como mais testes de remocao de vertice

--- grafo.py
+++ grafo.py
@@ -1,6 +1,6 @@
 
 #vertices e um dict{nome:Vertice}
-#arestas e um dict{vertice v1:Lista<Vertice>} que representa que existe uma aresta entre v1 e todos os vertices da lista
+#arestas e um dict{nome vertice:set<Vertice>} que representa que existe uma aresta entre v1 e todos os vertices da lista
 class Grafo:
 
     def __init__(self):
@@ -34,12 +34,35 @@ class Grafo:
         adjacentes_v2.remove(vertice1)
 
     def estao_conectados(self, vertice1, vertice2):
-        adjacentes_v1 = self._arestas[vertice1.nome]
-        adjacentes_v2 = self._arestas[vertice2.nome]
-        return vertice2 in adjacentes_v1 and vertice1 in adjacentes_v2
+        try:
+            adjacentes_v1 = self._arestas[vertice1.nome]
+            adjacentes_v2 = self._arestas[vertice2.nome]
+            return vertice2 in adjacentes_v1 and vertice1 in adjacentes_v2
+        except KeyError:
+            return False
 
     def vertices(self):
         return self._vertices.copy()
+
+    def vertice_qualquer(self):
+        for vertice in self._vertices:
+            return self._vertices[vertice]
+
+    def adjacentes(self, vertice):
+        return self._arestas[vertice.nome].copy()
+
+    def grau(self, vertice):
+        return len(self.adjacentes(vertice))
+
+    def eRegular(self):
+        if len(self._vertices) == 0:
+            return False
+        vertice1 = self.vertice_qualquer()
+        grau = self.grau(vertice1)
+        for nome in self._vertices:
+            if not (self.grau(self._vertices[nome]) == grau):
+                return False
+        return True
 
 class Vertice:
 
--- grafo_teste.py
+++ grafo_teste.py
@@ -8,6 +8,15 @@ class GrafoTeste(unittest.TestCase):
     def setUp(self):
         self.grafo = Grafo()
 
+    def adicionar_vertices(self):
+        a = Vertice('a')
+        b = Vertice('b')
+        c = Vertice('c')
+        conj = {'a':a, 'b':b, 'c':c}
+        for key in conj:
+            self.grafo.adicionar_vertice(conj[key])
+        return conj
+
     def teste_adicionar_vertice_aumenta_tamanho(self):
         self.grafo.adicionar_vertice(Vertice('a'))
         self.assertEqual(self.grafo.ordem(), 1)
@@ -18,6 +27,14 @@ class GrafoTeste(unittest.TestCase):
         self.grafo.remover_vertice(a)
         self.assertEqual(self.grafo.ordem(), 0)
 
+    def teste_remover_vertice_remove_arestas_conectadas_nele(self):
+        conj = self.adicionar_vertices()
+        self.grafo.conectar(conj['a'], conj['b'])
+        self.grafo.conectar(conj['b'], conj['c'])
+        self.grafo.remover_vertice(conj['a'])
+        self.assertFalse(self.grafo.estao_conectados(conj['a'], conj['b']))
+        self.assertTrue(self.grafo.estao_conectados(conj['b'], conj['c']))
+
     def teste_conectar_dois_vertices(self):
         a = Vertice('a')
         b = Vertice('b')
@@ -25,6 +42,51 @@ class GrafoTeste(unittest.TestCase):
         self.grafo.adicionar_vertice(b)
         self.grafo.conectar(a,b)
         self.assertTrue(self.grafo.estao_conectados(a,b))
+
+    def teste_ordem(self):
+        self.adicionar_vertices()
+        self.assertEqual(self.grafo.ordem(), 3)
+
+    def teste_obter_todos_os_vertices(self):
+        conj = self.adicionar_vertices()
+        self.assertDictEqual(self.grafo.vertices(), conj)
+
+    def teste_adjacentes(self):
+        conj = self.adicionar_vertices()
+        a = conj['a']
+        b = conj['b']
+        c = conj['c']
+        self.grafo.conectar(a,b)
+        self.grafo.conectar(a,c)
+        self.assertSetEqual(self.grafo.adjacentes(a), {b,c})
+
+    def teste_grau_de_vertice(self):
+        conj = self.adicionar_vertices()
+        a = conj['a']
+        b = conj['b']
+        c = conj['c']
+        self.grafo.conectar(a,b)
+        self.grafo.conectar(a,c)
+        self.assertEqual(self.grafo.grau(a), 2)
+
+    def teste_grafo_vazio_nao_e_regular(self):
+        grafo = Grafo()
+        self.assertFalse(grafo.eRegular())
+
+    def teste_nao_regular(self):
+        conj = self.adicionar_vertices()
+        self.grafo.conectar(conj['a'], conj['b'])
+        self.grafo.conectar(conj['a'], conj['c'])
+        self.assertFalse(self.grafo.eRegular())
+
+    def teste_e_regular(self):
+        grafoRegular = Grafo()
+        a = Vertice('a')
+        b = Vertice('b')
+        c = Vertice('c')
+        grafoRegular._vertices = {'a':a,'b':b,'c':c}
+        grafoRegular._arestas = {'a':{b,c}, 'b':{a,c}, 'c':{a,b}}
+        self.assertTrue(grafoRegular.eRegular())
 
 if __name__ == '__main__':
     unittest.main()
